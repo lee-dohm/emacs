2919c5f0802a6990b175b134a0c6876584a0ed31
Lee Dohm <lee@liftedstudios.com>
2013-11-13 19:31:18 -0800

    Add spell checking in text mode

--- custom/04flyspell.el
+++ custom/04flyspell.el
@@ -1,4 +1,10 @@
 
+;; Turns on spell checking in text mode
+(dolist (hook '(text-mode-hook))
+  (add-hook hook (lambda () (flyspell-mode 1))))
+(dolist (hook '(change-log-mode-hook log-edit-mode-hook))
+  (add-hook hook (lambda () (flyspell-mode -1))))
+
 ;; Turns on spell checking for comments and strings in the following languages
 (mapcar (lambda (mode-hook) (add-hook mode-hook 'flyspell-prog-mode))
         '(emacs-lisp-mode-hook
