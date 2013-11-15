5cf63d61a50d20119b4850173ed9252938f3f4f5
Lee Dohm <lee@liftedstudios.com>
2013-11-15 09:11:40 -0800

    A railscasts theme that works?

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Lee's Emacs environment
 
-This is my Emacs environment, such as it is. It is almost completely based off of [Setting up Emacs as Ruby development environment on OSX](http://crypt.codemancers.com/posts/2013-09-26-setting-up-emacs-as-development-environment-on-osx/).
+This is my Emacs environment, such as it is. It was originally based off of [Setting up Emacs as Ruby development environment on OSX](http://crypt.codemancers.com/posts/2013-09-26-setting-up-emacs-as-development-environment-on-osx/) but I've since added a number of tweaks and configurations that I've picked up from a lot of other places.
 
 ## Prerequisites
 
--- themes/railscasts-theme.el
+++ themes/railscasts-theme.el
@@ -1,45 +1,38 @@
 
 (deftheme railscasts
-  "Based on the great Railscasts theme by Ryan Bates")
+  "Theme for faces, based on the Railscasts textmate theme by Ryan Bates.")
 
-(let 
-  (custom-theme-set-faces
-   'railscasts
-   `(default ((,class (:foreground "#f8f8f8" :background "#282828"))))
-   `(cursor ((,class (:background "#5a647e"))))
-   `(region ((,class (:background "#555577"))))
-   `(fringe ((,class (:background "#282828"))))
-   `(highlight ((,class (:background "darkolivegreen"))))
-   `(primary-selection ((,class (:background "#555577"))))
-   `(secondary-selection ((,class (:background "darkslateblue"))))
-   `(isearch ((,class (:background "#555555"))))
-   `(minibuffer-prompt ((,class (:weight bold :background "#ff6600"))))
+(custom-theme-set-faces
+ 'railscasts
+ '(default ((t (:foreground "#e6e1dc" :background "#2b2b2b"))))
+ '(cursor ((t (:foreground "#444444" :background "#ffffff"))))
+ '(highlight ((t (:background "#333435"))))
+ '(region ((t (:background "#333435"))))
+ '(font-lock-builtin-face ((t (:foreground "#d0d0ff"))))
+ '(font-lock-comment-face ((t (:foreground "#555555"))))
+ '(font-lock-constant-face ((t (:foreground "#6d9cbe"))))
+ '(font-lock-function-name-face ((t (:foreground "#ffc66d"))))
+ '(font-lock-keyword-face ((t (:foreground "#cc7833"))))
+ '(font-lock-string-face ((t (:foreground "#a5c261"))))
+ '(font-lock-type-face ((t (:foreground "#ffffff"))))
+ '(font-lock-variable-name-face ((t (:foreground "#d0d0ff"))))
+ '(font-lock-warning-face ((t (:foreground "#990000"))))
+ '(erb-out-delim-face ((t (:foreground "#E6E1DC"))))
+ '(erb-delim-face ((t (:foreground "#E6E1DC" ))))
+ '(erb-exec-face ((t (:background "#191919"))))
+ '(erb-out-face ((t (:background "#191919"))))
 
-   `(font-lock-builtin-face ((,class (:foreground "#d0d0ff"))))
-   `(font-lock-comment-face ((,class (:foreground "#bc9458"))))
-   `(font-lock-constant-face ((,class (:foreground "#6d9cbe"))))
-   `(font-lock-doc-string-face ((,class (:foreground "#a5c261"))))
-   `(font-lock-function-name-face ((,class (:foreground "#ffc66d"))))
-   `(font-lock-keyword-face ((,class (:foreground "#cc7833"))))
-   `(font-lock-preprocessor-face ((,class (:foreground "#cc7833"))))
-   `(font-lock-reference-face ((,class (:foreground "LightSteelBlue"))))
-   `(font-lock-string-face ((,class (:foreground "#a5c261"))))
-   `(font-lock-type-face ((,class (:foreground "white"))))
-   `(font-lock-variable-name-face ((,class (:foreground "LightSteelBlue"))))
-   `(font-lock-warning-face ((,class (:foreground "Pink"))))
-
-   `(enh-ruby-op-face ((,class (:foreground "#cc7833"))))
-   `(enh-ruby-string-delimiter-foce ((,class (:foreground "#a5c261"))))
-
-   `(paren-face-match-light ((,class (:foreground "#ffc66d" :background "#555577"))))
-   `(modeline ((,class (:background "#a5baf1" :foreground "black"))))
-   `(modeline-buffer-id ((,class (:background "#a5baf1" :foreground "black"))))
-   `(modeline-mousable ((,class (:background "#a5baf1" :foreground "black"))))
-   `(modeline-mousable-minor-mode ((,class (:background "#a5baf1" :foreground "black")))))
-
-  (custom-theme-set-variables
-   'railscasts))
+ '(button ((t (:underline t :foreground "#6d9cbe"))))
+ '(link ((t (:underline t :foreground "#6d9cbe"))))
+ '(link-visited ((t (:underline t :foreground "#6d9cbe"))))
+ '(mode-line ((t (:box nil
+                 :background "#1c1c1c" :foreground "#444444"))))
+ '(mode-line-inactive ((t (:box nil
+                                :background "#1f1f1f" :foreground "#333333"))))
+ '(isearch ((t (:background "#555555"))))
+ '(minibuffer-prompt ((t (:bold t :foreground "#cc7833"))))
+ '(show-paren-match-face ((t (:foreground "#444444" :background "#cc7833"))))
+ '(show-paren-mismatch-face ((t (:foreground "#444444" :background "#990000"))))
+ '(lazy-highlight ((t (:background "#cc7833")))))
 
 (provide-theme 'railscasts)
-
-
