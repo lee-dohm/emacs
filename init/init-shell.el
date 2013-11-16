35b3d98b0add29e61ba91c4108e6f1669889f58e
Lee Dohm <lee@liftedstudios.com>
2013-11-16 14:41:02 -0800

    Tweak the shell settings so projectile works correctly

--- init/init-shell.el
+++ init/init-shell.el
@@ -2,4 +2,10 @@
 ;; Copyright (c) 2013 by Lee Dohm. All Rights Reserved.
 ;;
 
+(setq-default explicit-shell-file-name "/bin/bash")
+(setq-default shell-file-name "/bin/bash")
+
+(setenv "PATH" (concat "/usr/local/bin:" (getenv "PATH")))
+(add-to-list 'exec-path "/usr/local/bin")
+
 (add-to-list 'auto-mode-alist '("\\.fish\\'" . shell-script-mode))
