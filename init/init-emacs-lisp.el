84351c47a71f6f2de04cee58b2e61cc020b39bdb
Lee Dohm <lee@liftedstudios.com>
2013-11-16 14:41:33 -0800

    Make eld files open emacs-lisp-mode

--- init/init-emacs-lisp.el
+++ init/init-emacs-lisp.el
@@ -1,2 +1,6 @@
+;;
+;; Copyright (c) 2013 by Lee Dohm. All Rights Reserved.
+;;
 
 (add-to-list 'auto-mode-alist '("Cask\\'" . emacs-lisp-mode))
+(add-to-list 'auto-mode-alist '("\\.eld'" . emacs-lisp-mode))
