1114909d4c8b38d0ba7ad9ab3aa58de8e7285d24
Lee Dohm <lee@liftedstudios.com>
2013-11-17 12:02:52 -0800

    Fix the broken ruby-mode autoload specifications

--- init/init-ruby.el
+++ init/init-ruby.el
@@ -4,3 +4,9 @@
 
 (add-hook 'ruby-mode-hook 'yard-mode)
 (add-hook 'ruby-mode-hook (lambda () (interactive) (column-marker-1 100)))
+
+(add-to-list 'auto-mode-alist '("Gemfile\\'" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Guardfile\\'" . ruby-mode))
+(add-to-list 'auto-mode-alist '("[rR]akefile\\'" . ruby-mode))
+(add-to-list 'auto-mode-alist '("\\.rake\\'" . ruby-mode))
+(add-to-list 'auto-mode-alist '("\\.ru\\'" . ruby-mode))
