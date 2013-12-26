1c06d602d0017b04f59eeadf35c40279ba29082c
Lee Dohm <lee@liftedstudios.com>
2013-12-25 23:39:25 -0800

    Add Vagrantfile to the list of Ruby file types

--- init.d/ruby.el
+++ init.d/ruby.el
@@ -11,6 +11,7 @@
 (add-to-list 'auto-mode-alist '("Gemfile\\'" . ruby-mode))
 (add-to-list 'auto-mode-alist '("Guardfile\\'" . ruby-mode))
 (add-to-list 'auto-mode-alist '("[rR]akefile\\'" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Vagrantfile\\'" . ruby-mode))
 (add-to-list 'auto-mode-alist '("\\.rake\\'" . ruby-mode))
 (add-to-list 'auto-mode-alist '("\\.ru\\'" . ruby-mode))
 (add-to-list 'auto-mode-alist '("pryrc\\'" . ruby-mode))
