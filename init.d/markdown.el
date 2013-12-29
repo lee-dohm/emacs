05e22fb3b2fc828f95edb2adaf2141955699aacf
Lee Dohm <lee@liftedstudios.com>
2013-12-29 13:09:58 -0800

    Map cursor movement keys properly in markdown-mode

--- init.d/markdown.el
+++ init.d/markdown.el
@@ -1,3 +1,9 @@
 (require 'markdown-mode)
 (setq markdown-command "/usr/local/bin/multimarkdown")
 (setq markdown-css-path (expand-file-name "data/markdown.css" lcd-emacs-config-dir))
+
+(defun lcd-markdown-mode-keymappings ()
+  (local-set-key (kbd "M-<right>") 'right-word)
+  (local-set-key (kbd "M-<left>") 'left-word))
+
+(add-hook 'markdown-mode-hook 'lcd-markdown-mode-keymappings)
