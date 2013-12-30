4f7aa25f9f865391f8c2504de3518527cf50541b
Lee Dohm <lee@liftedstudios.com>
2013-12-29 17:42:12 -0800

    Flesh out Railscasts theme to full package definition

--- themes/railscasts-theme.el
+++ themes/railscasts-theme.el
@@ -1,9 +1,40 @@
-;;
-;; Copyright (c) 2013 by Lee Dohm. All Rights Reserved.
-;;
+;;; railscasts-theme.el --- A dark, medium-contrast color theme for Emacs.
 
-(deftheme railscasts
-  "Based on the Railscasts textmate theme by Ryan Bates.")
+;; Copyright (c) 2013 by Lee Dohm. All Rights Reserved.
+
+;; Author: Lee Dohm <lee@lee-dohm.com>
+;; URL: https://github.com/lee-dohm/railscasts-emacs
+;; Version: 0.5
+
+;; MIT License
+
+;; Permission is hereby granted, free of charge, to any person obtaining
+;; a copy of this software and associated documentation files (the
+;; "Software"), to deal in the Software without restriction, including
+;; without limitation the rights to use, copy, modify, merge, publish,
+;; distribute, sublicense, and/or sell copies of the Software, and to
+;; permit persons to whom the Software is furnished to do so, subject to
+;; the following conditions:
+
+;; The above copyright notice and this permission notice shall be
+;; included in all copies or substantial portions of the Software.
+
+;; THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+;; EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+;; MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+;; NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+;; LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+;; OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+;; WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+;;; Commentary:
+
+;; A port of the Railscasts theme created by Ryan Bates for the TextMate editor
+;; to use with Emacs 24.
+
+;;; Code:
+
+(deftheme railscasts "The Railscasts color theme")
 
 (let ((foreground "#e6e1dc")
       (background "#282828")
