919db532d322a7fd31341807826c93c6a5297c69
Lee Dohm <lee@liftedstudios.com>
2014-01-03 08:50:42 -0800

    Disable electric-indent-mode in markdown-mode

--- init.d/markdown.el
+++ init.d/markdown.el
@@ -2,8 +2,9 @@
 (setq markdown-command "/usr/local/bin/multimarkdown")
 (setq markdown-css-path (expand-file-name "data/markdown.css" user-emacs-directory))
 
-(defun lcd-markdown-mode-keymappings ()
+(defun lcd-markdown-mode-customizations ()
   (local-set-key (kbd "M-<right>") 'right-word)
-  (local-set-key (kbd "M-<left>") 'left-word))
+  (local-set-key (kbd "M-<left>") 'left-word)
+  (set (make-local-variable 'electric-indent-mode) nil))
 
-(add-hook 'markdown-mode-hook 'lcd-markdown-mode-keymappings)
+(add-hook 'markdown-mode-hook 'lcd-markdown-mode-customizations)
